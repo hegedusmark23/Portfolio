1cd4fb0f380cf2a5bf3bf2fa6ad6305c5f02d5bd
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-05-13 18:59:54 +0200

    new component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, AboveTheFoldComponent]
+    imports: [RouterOutlet, AboveTheFoldComponent, AboutMeComponent]
 })
 export class AppComponent {
   title = 'Mark Hegedus';
