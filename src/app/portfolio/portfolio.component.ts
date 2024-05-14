96089fcd05ef8a177808da277e23108eb599faf6
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-05-14 18:39:47 +0200

    small change

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -14,19 +14,22 @@ export class PortfolioComponent {
       name: 'Join',
       image: '',
       techstack: 'Angular | Typescript | HTML | CSS | Firebase',
-      description: 'Task Manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories'
+      description: 'Task Manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories',
+      style: 'row'
     },
     {
       name: 'El Pollo Loco',
       image: '',
       techstack: 'JavaScript | HTML | CSS ',
-      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to collect coins, and defeat the chikens.'
+      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to collect coins, and defeat the chikens.',
+      style: 'row-reverse'
     },
     {
       name: 'Pokedex',
       image: '',
       techstack: 'JavaScript | HTML | CSS | Api ',
-      description: 'Based on the PokéAPI a simple library that provides and catalouges pokemon information.'
+      description: 'Based on the PokéAPI a simple library that provides and catalouges pokemon information.',
+      style: 'row'
     }
   ]
 }
