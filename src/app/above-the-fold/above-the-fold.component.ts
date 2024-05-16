f7ee5c3c7c3b2298623587139ace6db2970a1127
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-05-16 18:40:53 +0200

    css changes, new components added

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
 
 @Component({
     selector: 'app-above-the-fold',
     standalone: true,
     templateUrl: './above-the-fold.component.html',
     styleUrl: './above-the-fold.component.scss',
-    imports: [HeaderComponent]
+    imports: []
 })
 export class AboveTheFoldComponent {
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
-import { AboutMeComponent } from "./about-me/about-me.component";
-import { MySkillsComponent } from "./my-skills/my-skills.component";
-import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { MainContentComponent } from "./main-content/main-content.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, AboveTheFoldComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent]
+    imports: [RouterOutlet, FooterComponent, HeaderComponent, MainContentComponent]
 })
 export class AppComponent {
   title = 'Mark Hegedus';
