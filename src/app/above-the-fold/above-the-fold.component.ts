5b2afbeb6d6e42d98345f3edfe245f6f0133fb55
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-05-20 12:09:43 +0200

    small changes on CSS/design

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -8,5 +8,64 @@ import { Component } from '@angular/core';
     imports: []
 })
 export class AboveTheFoldComponent {
+    txtFull = "FRONTEND DEVELOPER"; 
+    txtArr = this.txtFull.split("");
+    charsToLeave = Math.floor(Math.random() * 2) + 1;
 
+    constructor() {
+        if (typeof document !== 'undefined') {
+            this.writeOut(this.txtArr);
+        }
+    }
+
+    getRandomTime() {
+        return Math.random() * 0.5;
+    }
+
+    getEle(id: string) {
+        return typeof document !== 'undefined' ? document.getElementById(id) : null;
+    }
+
+    ModifyTxt(el: HTMLElement, txt: string) {
+        el.innerHTML = txt;
+    }
+
+    writeOut(txtArr: string[], curChar = 0) {
+        const randomTime = this.getRandomTime();
+        setTimeout(() => {
+            const el = this.getEle("text");
+            if (el) { 
+                let elTxt = el.innerHTML;
+                elTxt += txtArr[curChar];
+                this.ModifyTxt(el, elTxt);
+
+                if (curChar + 1 === txtArr.length) {
+                    return this.deleteOut(txtArr, txtArr.length);
+                }
+                return this.writeOut(txtArr, curChar + 1);
+            }
+        }, randomTime * 1000);
+    }
+
+    deleteOut(txtArr: string[], curChar: number) {
+        const randomTime = this.getRandomTime();
+        setTimeout(() => {
+            const el = this.getEle("text");
+            if (el) { 
+                let elTxt = el.innerHTML;
+
+                const elTxtArr = elTxt.split("");
+                const newElTxt = elTxtArr.slice(0, curChar - 1).join("");
+                this.ModifyTxt(el, newElTxt);
+
+                if (curChar - 1 <= this.charsToLeave) {
+                    return this.writeOut(txtArr, this.charsToLeave);
+                }
+                return this.deleteOut(txtArr, curChar - 1);
+            }
+        }, randomTime * 1000);
+    }
 }
+
+
+
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -16,7 +16,9 @@ export class PortfolioComponent {
       techstack: 'Angular | Typescript | HTML | CSS | Firebase',
       description: 'Task Manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories',
       flex_style: 'row',
-      alignment: 'start'
+      alignment: 'start',
+      github_link: 'https://github.com/hegedusmark23/Join',
+      test_link: 'https://portfolio.mark-hegedus.com/join/'
     },
     {
       name: 'El Pollo Loco',
@@ -24,7 +26,9 @@ export class PortfolioComponent {
       techstack: 'JavaScript | HTML | CSS ',
       description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to collect coins, and defeat the chikens.',
       flex_style: 'row-reverse',
-      alignment: 'end'
+      alignment: 'end',
+      github_link: 'https://github.com/hegedusmark23/El-Pollo-Loco',
+      test_link: 'https://portfolio.mark-hegedus.com/el_pollo_loco/'
     },
     {
       name: 'Pokedex',
@@ -32,7 +36,9 @@ export class PortfolioComponent {
       techstack: 'JavaScript | HTML | CSS | Api ',
       description: 'Based on the PokéAPI a simple library that provides and catalouges pokemon information.',
       flex_style: 'row',
-      alignment: 'start'
+      alignment: 'start',
+      github_link: 'https://github.com/hegedusmark23/Pokedex',
+      test_link: 'https://portfolio.mark-hegedus.com/pokedex/'
     }
   ]
 }
