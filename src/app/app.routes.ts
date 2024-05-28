8ebc62444ee26dde494c81e5593a49600dcb1ca2
Hegedüs Mark <hegedusmark@Hegedus-MacBook-Air.local>
2024-05-28 13:06:34 +0200

    impressum and privacy policy added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainContentComponent},
+    { path: 'impressum', component: ImpressumComponent},
+    { path: 'privacy', component: PrivacyPolicyComponent}
+];
