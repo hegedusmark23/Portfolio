a4e0b3d658c604236e53231cd42b6e32049d85d5
Hegedüs Mark <hegedusmark@Hegedus-MacBook-Air.local>
2024-05-30 13:10:34 +0200

    burger-menu added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,17 +2,20 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../app-translate/translation.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BurgerMenuComponent } from "../burger-menu/burger-menu.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [TranslateModule, CommonModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
+    imports: [TranslateModule, CommonModule, FormsModule, BurgerMenuComponent]
 })
 export class HeaderComponent {
   focusEffect: number | null = null;
   activeLang: number | null = 1;
+model: any;
 constructor(private translationService: TranslationService){}
 
 setLanguage(lang: string, index: number) {
