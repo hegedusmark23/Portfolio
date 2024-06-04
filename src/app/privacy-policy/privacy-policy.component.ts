3921010173bceb46e493cbe029be1dea7b7dfa76
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-06-04 19:02:35 +0200

    work on responsiveness

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -9,6 +9,26 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
-export class PrivacyPolicyComponent {
+
+export class PrivacyPolicyComponent implements OnInit {
   imgSrc: string = '../../assets/img/design-components/goup-btn.png';
+  dataTypesList!: string[];
+  categoriesList!: string[];
+  purposesList!: string[];
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit() {
+    this.translate.get('privacyPolicy.dataTypesList').subscribe((res: any) => {
+      this.dataTypesList = Array.isArray(res) ? res : [];
+    });
+
+    this.translate.get('privacyPolicy.categoriesList').subscribe((res: any) => {
+      this.categoriesList = Array.isArray(res) ? res : [];
+    });
+
+    this.translate.get('privacyPolicy.purposesList').subscribe((res: any) => {
+      this.purposesList = Array.isArray(res) ? res : [];
+    });
+  }
 }
