2a7a69b403899072457dd142d4e4b569667d5689
Mark Hegedus <hegeee23@hotmail.com>
2024-09-11 21:03:36 +0200

    portfolio updated

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -11,49 +11,38 @@ export class PortfolioComponent{
 
   portfolio = [
     {
+    name: 'DABubble',
+      image: 'dabubble.png',
+      techstack: 'Angular | Typescript | HTML | CSS | Firebase',
+      description: 'daBubbleDescription',
+      flex_style: 'row-reverse',
+      alignment: 'end',
+      github_link: 'https://github.com/hegedusmark23/dabubble',
+      test_link: 'https://dabubble.mark-hegedus.com/',
+      fade: 'fade-left'
+    },
+    {
       name: 'Join',
       image: 'join.png',
-      techstack: 'Angular | Typescript | HTML | CSS | Firebase',
+      techstack: 'JavaScript | HTML | CSS ',
       description: 'joinDescription',
       flex_style: 'row',
       alignment: 'start',
       github_link: 'https://github.com/hegedusmark23/Join',
       test_link: 'https://portfolio.mark-hegedus.com/join/',
-      fade: 'fade-left'
+      fade: 'fade-right'
     },
     {
       name: 'El Pollo Loco',
       image: 'pollo-loco.png',
-      techstack: 'JavaScript | HTML | CSS ',
+      techstack: 'JavaScript | HTML | CSS',
       description: 'elPolloLocoDescription',
       flex_style: 'row-reverse',
       alignment: 'end',
       github_link: 'https://github.com/hegedusmark23/El-Pollo-Loco',
       test_link: 'https://portfolio.mark-hegedus.com/el_pollo_loco/',
-      fade: 'fade-right'
-    },
-    {
-      name: 'Pokedex',
-      image: 'pokedex.png',
-      techstack: 'JavaScript | HTML | CSS | Api ',
-      description: 'pokedexDescription',
-      flex_style: 'row',
-      alignment: 'start',
-      github_link: 'https://github.com/hegedusmark23/Pokedex',
-      test_link: 'https://portfolio.mark-hegedus.com/pokedex/',
       fade: 'fade-left'
-    },
-    /*{
-      name: 'Simple CRM',
-      image: 'simplecrm.png',
-      techstack: 'Angular | Firebase ',
-      description: 'simpleCrmDescription',
-      flex_style: 'row-reverse',
-      alignment: 'end',
-      github_link: 'https://github.com/hegedusmark23/SimpleCRM',
-      test_link: 'https://portfolio.mark-hegedus.com/simplecrm/',
-      fade: 'fade-right'
-    }*/
+    }
   ]
 
 
